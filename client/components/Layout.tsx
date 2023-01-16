8ffbc0b60877b2d1a40a9189134f52c519d21591
Gianni Cafano <gcafano@gmail.com>
2023-01-16 19:19:10 +0000

    feat: add layout component

---
+++ client/components/Layout.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Footer from './Footer';
+import Header from './Header';
+
+import { StyledLayoutMain } from '../styles/Layout.styles'
+
+type LayoutProps = {
+    children: React.ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps): React.ReactElement {
+  return (
+    <>
+        <Header />
+            <StyledLayoutMain>
+                {children}
+            </StyledLayoutMain>
+        <Footer />
+    </>
+  )
+}
