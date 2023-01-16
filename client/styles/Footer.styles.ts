5d5e23b1dd3d3cdf004eb29a91e2c9f405ed6e00
Gianni Cafano <gcafano@gmail.com>
2023-01-16 19:17:58 +0000

    feat: add global footer for product page

---
+++ client/styles/Footer.styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const StyledFooter = styled.footer`
+    background-color: ${props => props.theme.colors.hemocyanin};
+    padding: 1.5rem;
+`
+
+export const StyledFooterParagraph = styled.p`
+    margin: 0;
+    font-size: 1.2rem;
+`
