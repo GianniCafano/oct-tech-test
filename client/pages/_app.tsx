64d0705d78b6f481fb569d491eecc526d0502a91
Gianni Cafano <gcafano@gmail.com>
2023-01-16 19:16:43 +0000

    feat: wrap root application in theme provider

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,17 @@
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+import { theme } from '../styles/theme.styles'
 import "../styles/globals.css";
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+  )
 }
 
 export default MyApp;
